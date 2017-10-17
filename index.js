50fa29bad3fa8119a94f08e4f01a0173f3ca5f5e
Rebecca Stevens <rsbjshan@gmail.com>
2017-10-17 11:13:07 +0100

    Added $(this) to target specific element

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $(".navigations").mouseenter(function(){
-        $(".navigations").addClass("underline");
+        $(this).addClass("underline");
     });
     $(".navigations").mouseleave(function(){
-        $(".navigations").removeClass("underline");
+        $(this).removeClass("underline");
     });
 });
